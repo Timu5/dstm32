e4193e2679d4a1dd857715b2c7f77d448687f433
Timu5 <timusziomek@gmail.com>
2019-05-10 14:58:15 +0200

    Add input module

---
+++ source/input.d
@@ -0,0 +1,22 @@
+module input;
+
+import stm32f4xx;
+
+enum Button
+{
+    up = 0,
+    down,
+    left,
+    right
+}
+
+void init()
+{
+    RCC.AHB1ENR |= RCC_AHB1Periph_GPIOD; // power up gpiod
+    GPIOD.PUPDR |= (1 << (0 * 2)) | (1 << (1 * 2)); // enable pull up
+}
+
+bool getButton(Button button)
+{
+    return (GPIOD.IDR.load() & (1 << button)) != 0;
+}
